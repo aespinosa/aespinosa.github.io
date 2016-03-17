a79ca57793181d9d28eadff148c2e24fdb8a16dc
Allan Espinosa <allan.espinosa@outlook.com>
2016-03-17 11:52:30 -0500

    Describe the build environment in Nix

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+  haskellPackages = pkgs.haskellPackages;
+  darwin = pkgs.darwin;
+in
+
+stdenv.mkDerivation {
+  name = "haskell-environment";
+  buildInputs =  [ 
+    (haskellPackages.ghcWithPackages (p: [ p.hakyll ] ))
+    darwin.apple_sdk.frameworks.Cocoa
+  ];
+}
