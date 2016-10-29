99ecd7d1fc7d147fd21441508c827fda94066034
Allan Espinosa <allan.espinosa@outlook.com>
2016-10-28 22:19:12 -0500

    Move normalize.css as a nix dependency

--- default.nix
+++ default.nix
@@ -1,14 +1,24 @@
 with import <nixpkgs> {};
+let
+  normalize = fetchurl {
+    url =  "https://raw.githubusercontent.com/necolas/normalize.css/3.0.1/normalize.css";
+    sha256 = "04xsqiacggsdbpvn3mv8azfh95rymn5lmmzjpml0cqm14czn3q4f";
+  };
+in
+
 
 stdenv.mkDerivation {
-  name = "haskell-environment";
+  name = "espinosa-io";
   buildInputs =  [ 
     (haskellPackages.ghcWithPackages (p: [
       (p.hakyll.overrideDerivation (old: {
         doCheck = false;
       }))
     ] ))
-    darwin.apple_sdk.frameworks.Cocoa
-    pkgs.nodePackages.less
+    lessc
   ];
+  inputs = [ normalize ];
+  shellHook = ''
+    ln -sf ${normalize} normalize.css
+  '';
 }
